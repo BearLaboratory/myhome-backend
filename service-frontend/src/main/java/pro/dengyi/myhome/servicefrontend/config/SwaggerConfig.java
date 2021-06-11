022e79029a7bc90054e202b0fbfb14e965cc9002
dengyi <dengyi@dengyi.pro>
2021-06-11 10:17:39 +0800

    修改swagger配置文件

--- service-frontend/src/main/java/pro/dengyi/myhome/servicefrontend/config/SwaggerConfig.java
+++ service-frontend/src/main/java/pro/dengyi/myhome/servicefrontend/config/SwaggerConfig.java
@@ -4,13 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * swagger配置文件
@@ -23,28 +29,33 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket createRestApi() {
+        List<Parameter> parameters = new ArrayList<>();
+        parameters.add(new ParameterBuilder()
+                .name("token")
+                .description("认证token")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build());
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
+                .globalOperationParameters(parameters)
                 .select()
-                // 为当前包路径
                 .apis(RequestHandlerSelectors.basePackage("com.palmap.serviceucenter.controller"))
                 .paths(PathSelectors.any())
                 .build();
-        //        return new
-        // Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class)).build();
     }
 
-    // 构建 api文档的详细信息函数,注意这里的注解引用的是哪个
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 // 页面标题
-                .title("哆啦A梦API文档")
+                .title("MyHome智能家居系统API文档")
                 // 创建人
                 .contact(new Contact("邓艺", "www.dengyi.pro", "dengyi@dengyi.pro"))
                 // 版本号
                 .version("1.0")
                 // 描述
-                .description("用户中心API文档")
+                .description("用户端API文档")
                 .build();
     }
 }
